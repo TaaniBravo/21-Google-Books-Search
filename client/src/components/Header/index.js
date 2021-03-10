6620448567d128ad474b8036ec4f539361199054
Taani Maama <taani1maama@live.com>
2021-03-09 18:40:56 -0800

    Created the Header component.

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">
+          <i class="fab fa-react"></i> Google Books
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/saved">Saved</Nav.Link>
+            <Nav.Link href="/">Search</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl
+              type="text"
+              placeholder="Quick Search"
+              className="mr-sm-2"
+            />
+            <Button variant="outline-success">Let's look!</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
