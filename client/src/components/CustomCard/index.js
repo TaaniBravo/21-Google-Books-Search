a8394c011bb9a3c7e15eaa36c35bb43f6de15944
Taani Maama <taani1maama@live.com>
2021-03-09 20:07:40 -0800

    Template version of the Saved page is ready.

--- client/src/components/CustomCard/index.js
+++ client/src/components/CustomCard/index.js
@@ -4,12 +4,13 @@ import { Card } from "react-bootstrap";
 const CustomCard = () => {
   return (
     <div>
-      <Card style={{ width: "18rem" }}>
+      <Card style={{ width: "100%" }}>
         <Card.Body>
           <Card.Title>Card Title</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
             Card Subtitle
           </Card.Subtitle>
+          <Card.Img variant="top" src="" />
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import Hero from "../../components/Hero";
+import { List, ListItem } from "../../components/List";
+import CustomCard from "../../components/CustomCard";
 
 const Saved = () => {
   return (
-    <div>
+    <main>
       <Hero />
-    </div>
+      <Container>
+        <h2>Saved Books</h2>
+        <List>
+          <ListItem>
+            <CustomCard></CustomCard>
+          </ListItem>
+        </List>
+      </Container>
+    </main>
   );
 };
 
