1e06c9ead19b5875787deaf73c609d945a75d8b5
Taani Maama <taani1maama@live.com>
2021-03-11 16:41:05 -0800

    Created the basic booksController that we will need.

---
+++ controllers/booksController.js
@@ -0,0 +1,29 @@
+const db = require("../models");
+
+// Defining methods for the booksController
+module.exports = {
+  findAll: async (req, res) => {
+    try {
+      const dbModel = await db.Book.find(req.query).sort({ date: -1 });
+      res.json(dbModel);
+    } catch (err) {
+      res.status(422).json(err);
+    }
+  },
+  findById: async (req, res) => {
+    try {
+      const dbModel = await db.Book.findById(req.params.id);
+      res.json(dbModel);
+    } catch (err) {
+      res.status(422).json(err);
+    }
+  },
+  remove: async (req, res) => {
+    try {
+      const dbModel = db.Book.findById({ _id: req.params.id });
+      res.json(dbModel.remove());
+    } catch (error) {
+      res.status(422).json(err);
+    }
+  }
+};
